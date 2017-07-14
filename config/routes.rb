422415efacf461f3a1a3fa8cabee764e7b43ee70
Kevin Wong <kevinwongtyl@gmail.com>
2017-07-14 23:57:19 +0000

    Fixed routes.rb so that 'Rails.application.routes.draw do' is called correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-MsprayMonitorApp.application.routes.draw do
+Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
