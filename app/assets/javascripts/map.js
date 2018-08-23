b34df98580fd4ccb560280de10fdcc8f677050d4
Daniel Wu <ldanielcwu@gmail.com>
2018-08-23 15:26:38 -0700

    Clean up map.js JS code

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,24 +1,23 @@
 function initMap() {
+    // Create the new map.
     var mapSettings = {
         zoom: 8,
         center: new google.maps.LatLng(-23.151, 30.658)
     };
-    
     var mapDiv = document.getElementById('map');
     var map = new google.maps.Map(mapDiv, mapSettings);
-    var infowindow = new google.maps.InfoWindow({
-        content: '' 
-    });
- 
-    function createMarker()
-    {
+    var infowindow = new google.maps.InfoWindow({ content: '' });
+
+    // Create an array of new markers.
+    function createMarker() {
         var marker = new google.maps.Marker({
             position: {lat: gon.data[i]["lat"], lng: gon.data[i]["lon"]},
             map: map
         });
 
-        var contentString = '<div>' + gon.data[i]["lat"].toString() + ', ' + 
-            gon.data[i]["lon"].toString() + '</div>';
+        var contentString = gon.data[i]["lat"].toString();
+        contentString += ', ' + gon.data[i]["lon"].toString();
+        contentString = '<div>' + contentString + '</div>';
 
         marker.addListener('click', function() {
             infowindow.setContent(contentString);
@@ -29,11 +28,9 @@ function initMap() {
     }
 
     var markers = new Array();
-
     for(var i = 0; i < gon.data.length; i++)
-    {
         markers.push(createMarker());
-    }
 
+    // Add clustering for markers.
     var markerCluster = new MarkerClusterer(map, markers, {imagePath: "../assets/m"});
 }
