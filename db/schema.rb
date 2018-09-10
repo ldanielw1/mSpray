23e8b8c6be7b5f47969509317b6a4b88d9e6bcbf
Daniel Wu <ldanielcwu@gmail.com>
2018-09-10 10:51:06 -0700

    Updating DB

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20170713004004) do
     t.decimal "lat",                precision: 13, scale: 10, default: "0.0"
     t.decimal "lng",                precision: 13, scale: 10, default: "0.0"
     t.integer "gps_accuracy",                                 default: 0
-    t.boolean "homestead_sprayed",                            default: false
     t.boolean "is_mopup_spray",                               default: false
     t.string  "foreman",                                      default: ""
     t.string  "sprayers"
