10ba09ccb7378f6f5703736705470c83470f6896
Daniel Wu <ldanielcwu@gmail.com>
2018-09-11 16:44:10 -0700

    DB Migration Smashing to include future spray locations

--- db/migrate/20170713004004_initial_setup.rb
+++ db/migrate/20170713004004_initial_setup.rb
@@ -2,42 +2,47 @@ class InitialSetup < ActiveRecord::Migration[5.0]
   def change
 
     create_table :workers do |t|
-      t.integer  :worker_id
-      t.string   :name
-      t.boolean  :active
-
-      t.timestamps
+      t.string   :worker_id,                             :default => ""
+      t.string   :name,                                  :default => ""
+      t.boolean  :active,                                :default => true
     end
 
     create_table :spray_data do |t|
-      t.string  :imei,                                   :default => ""
       t.string  :timestamp,                              :default => ""
-      t.decimal :lat, :precision => 13, :scale => 10,    :default => 0.0
-      t.decimal :lng, :precision => 13, :scale => 10,    :default => 0.0
-      t.integer :gps_accuracy,                           :default => 0
-
-      t.boolean :is_mopup_spray,                         :default => false
+      t.string  :imei,                                   :default => ""
 
       t.string  :foreman,                                :default => ""
       t.string  :sprayers
       t.string  :chemical_used,                          :default => ""
-
+      t.string  :stats
       t.integer :unsprayed_rooms,                        :default => 0
       t.integer :unsprayed_shelters,                     :default => 0
-      t.string  :stats
+
+      t.boolean :is_mopup_spray,                         :default => false
+
+      t.decimal :lat, :precision => 13, :scale => 10,    :default => 0.0
+      t.decimal :lng, :precision => 13, :scale => 10,    :default => 0.0
+      t.integer :gps_accuracy,                           :default => 0
+    end
+
+    create_table :future_spray_locations do |t|
+      t.decimal :lat, :precision => 13, :scale => 10,    :default => 0.0
+      t.decimal :lng, :precision => 13, :scale => 10,    :default => 0.0
+      t.string :reporter,                                :default => ""
     end
 
     create_table :users do |t|
-      t.string  :provider
-      t.string  :uid
-      t.string  :name
-      t.string  :email
-      t.string  :profile_img
-      t.boolean :admin,         default: false
+      t.string  :name,                                   :default => ""
+      t.string  :email,                                  :default => ""
+      t.string  :profile_img,                            :default => ""
+      t.boolean :admin,                                  :default => false
+
+      t.string  :provider,                               :default => ""
+      t.string  :uid,                                    :default => ""
     end
 
     create_table :allowed_emails do |t|
-      t.string :email
+      t.string :email,                                   :default => ""
     end
 
   end
