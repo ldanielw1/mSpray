d18d34d63de44213a635eb94808a7e4f4fe8493e
Jermaine Zhang <jermainz@uci.edu>
2018-09-12 16:00:30 -0700

    Fixed feedback again.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,7 +8,7 @@ class DashboardController < ApplicationController
     end
 
     # Prep all FutureSprayLocation objects for displaying in map view.
-	  future_spray_locations = FutureSprayLocation.all.map do |data|
+    future_spray_locations = FutureSprayLocation.all.map do |data|
       attribute_hash = data.attributes
       ["lat", "lng"].each { |label| attribute_hash[label] = attribute_hash[label].to_f }
       attribute_hash
