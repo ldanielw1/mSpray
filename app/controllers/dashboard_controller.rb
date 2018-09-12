8e783129192524ebbb42dabf4d67b864155d199e
Daniel Wu <ldanielcwu@gmail.com>
2018-09-12 16:14:42 -0700

    Remove one last tab

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def show
-  	# Prep all SprayDatum objects for displaying in map view.
-  	spray_data = SprayDatum.all.map do |data|
+    # Prep all SprayDatum objects for displaying in map view.
+    spray_data = SprayDatum.all.map do |data|
       attribute_hash = data.attributes
       ["lat", "lng"].each { |label| attribute_hash[label] = attribute_hash[label].to_f }
       attribute_hash
@@ -24,9 +24,9 @@ class DashboardController < ApplicationController
   end
 
   def add_future_spray_location
-  	spray_location = FutureSprayLocation.new(lat: params[:lat], lng: params[:lng], reporter: params[:reporter])
-  	spray_location.save!
-	  redirect_back(fallback_location: dashboard_path)
+    spray_location = FutureSprayLocation.new(lat: params[:lat], lng: params[:lng], reporter: params[:reporter])
+    spray_location.save!
+    redirect_back(fallback_location: dashboard_path)
   end
 
   def delete_future_spray_location
