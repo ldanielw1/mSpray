937e3f3a45b3901fad393505e144cd343f8dc962
Kevin Wong <kevinwongtyl@gmail.com>
2018-09-13 10:41:18 -0700

    Updated branch to sync with master

--- app/helpers/spray_datum_helper.rb
+++ app/helpers/spray_datum_helper.rb
@@ -11,17 +11,7 @@ module SprayDatumHelper
     sd.chemical_used         = params[:chemical_used]
     sd.unsprayed_rooms       = params[:unsprayed_rooms]
     sd.unsprayed_shelters    = params[:unsprayed_shelters]
-
-    stats = Hash.new
-    params[:stats].each do |sprayer, s_stats|
-      stats[sprayer] = Hash.new
-      stats[sprayer][:rooms_sprayed] = s_stats["roomsSprayed"]
-      stats[sprayer][:shelters_sprayed] = s_stats["sheltersSprayed"]
-      stats[sprayer][:refilled] = s_stats["refilled"]
-    end
-
-    sd.stats = stats
-    sd.save
-
+    sd.stats                 = params[:stats]
+    sd.save!
   end
 end
