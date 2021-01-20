863d61170309ec10a5f57660919d08ba816020e3
Daniel Wu <danielwu@us.ibm.com>
2021-01-20 06:15:13 -0800

    Remove unused js file

--- app/assets/javascripts/modal_focus.js
+++
@@ -1,9 +0,0 @@
-/**
- * Focus the submit button on all modals when pulled up. No need to focus anything on modal close - it will refocuson trigger button by default.
- */
-$(window).on('shown.bs.modal', function(evt) {
-    modalButton = $(evt.relatedTarget);
-    modal = $(modalButton.attr("data-target"));
-    default_button = modal.find(".modal-default")
-    default_button.focus();
-});
